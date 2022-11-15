058b96ba201211b7f366b0b3ad519a495de90ed8
Felix Le <phuc.le@s3corp.com.vn>
2022-11-15 16:25:42 +0700

    add FILES route

--- main.js
+++ main.js
@@ -5,18 +5,17 @@ const URLS = [
 
   // { url: '/synapse/dashboard', paths: ['..', 'dashboard', 'build'] },
   // { url: '/chroma-app/dashboard', paths: ['..', 'razer-chroma-app', 'build'] },
-  // { url: '/synapse/products/128/ui', paths: ['..', 'port_ui', 'build'] },
-  // { url: '/synapse/products/128/mw', paths: ['..', 'evelyn_mw', 'build'] },
-  // { url: '/synapse/products/124/mw', paths: ['..', 'lilywireless_mw', 'build'] },
+  // { url: '/synapse/products/154/ui', paths: ['..', 'port_ui', 'build'] },
+  // { url: '/synapse/products/154/mw', paths: ['..', 'port_mw', 'build'] },
 
   // { url: '/synapse/dashboard', port: 3008 },
   // { url: '/synapse/settings', port: 3012 },
   // { url: '/synapse/profiles', port: 3005 },
-  // { url: '/synapse/gms-proxy', port: 3011 },
+  // { url: '/synapse/gms-proxy', port: 3010 },
   // { url: '/synapse/macro', port: 3009 },
   // { url: '/systray/systrayv2', port: 3007 },
-  // { url: '/synapse/products/128/ui', port: 3000 },
-  // { url: '/synapse/products/575/mw', port: 3001 },
+  // { url: '/synapse/products/154/ui', port: 3000 },
+  // { url: '/synapse/products/154/mw', port: 1154, pathRewrite: true },
 
   // { url: '/chroma-app/dashboard', port: 3001 },
   // { url: '/chroma-app/settings', port: 3002 },
@@ -25,6 +24,11 @@ const URLS = [
   // { url: '/synapse/audio-visualizer', port: 3009 },
   // { url: '/synapse/lighting-engine', port: 3011 },
 ];
+
+const FILES = [
+  // { path: '/synapse/dashboard', name: 'AvailableDevices.json' }
+];
+
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
@@ -33,12 +37,27 @@ const HOST = 'https://apps-staging.razer.com';
 
 app.listen(5000, () => console.log('Server started!'));
 
-URLS.forEach(({ url, paths, port }) => {
+URLS.forEach(({ url, paths, port, pathRewrite }) => {
   app.use(
     url,
     paths
       ? express.static(path.join(__dirname, ...paths))
-      : createProxyMiddleware({ target: `http://127.0.0.1:${port}` })
+      : createProxyMiddleware({
+          target: `http://127.0.0.1:${port}`,
+          pathRewrite: pathRewrite && { [url]: '' },
+        })
+  );
+});
+
+FILES.forEach(({ path, name }) => {
+  app.use(
+    `${path}/${name}`,
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:5000/test/',
+      pathRewrite: {
+        [path]: '',
+      },
+    })
   );
 });
 
