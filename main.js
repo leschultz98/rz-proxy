88a9b4724e13d1849daffc3da0d873aec80918dc
Felix Le <phuc.le@s3corp.com.vn>
2023-06-20 12:38:38 +0700

    feat: lighting-engine/manifest

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const URLS = [
   { url: '/test', paths: ['.', 'test'] },
   { url: '/installer', paths: ['..', 'installer-ts', 'build'] },
   { url: '/background-manager', paths: ['..', 'appengine-background-manager', 'build'] },
+  { url: '/synapse/lighting-engine/manifest', paths: ['..', 'razer-lighting-engine\\public\\manifest'] },
 
   // { url: '/synapse/dashboard', paths: ['..', 'dashboard', 'build'] },
   // { url: '/chroma-app/dashboard', paths: ['..', 'razer-chroma-app', 'build'] },
   // { url: '/synapse/products/154/ui', paths: ['..', 'port_ui', 'build'] },
   // { url: '/synapse/products/154/mw', paths: ['..', 'port_mw', 'build'] },
 
+  // { url: '/synapse/philips-hue', port: 3000 },
   // { url: '/synapse/dashboard', port: 3008 },
   // { url: '/synapse/settings', port: 3012 },
   // { url: '/synapse/profiles', port: 3005 },
@@ -17,7 +19,6 @@ const URLS = [
   // { url: '/systray/systrayv2', port: 3007 },
   // { url: '/remote-sync-worker', port: 5555, onlyLocalHost: true },
   // { url: '/synapse/products/99/ui', port: 3000 },
-  // { url: '/synapse/products/3878/ui', port: 3000 },
   { url: `/synapse/products/${MW_PID}/mw`, port: 1000 + MW_PID, pathRewrite: true },
 
   // { url: '/chroma-app/dashboard', port: 3001 },
@@ -58,26 +59,20 @@ ASSETS_HOST.forEach(({ prefix, target }) =>
     createProxyMiddleware({
       target,
       changeOrigin: true,
-      pathRewrite: {
-        [prefix]: '',
-      },
+      pathRewrite: { [prefix]: '' },
     })
   )
 );
 
 const FILES = [
   // { path: '/synapse/dashboard/AvailableDevices.json', name: 'dump.json' },
-  // { path: '/synapse/lighting-engine/manifest/DeviceManifest_3878_0.json', name: 'manifest.json' },
-  // { path: '/synapse/lighting-engine/manifest/DeviceManifest_3853_0.json', name: 'DeviceManifest_3853_0.json' },
 ];
 FILES.forEach(({ path, name }) =>
   app.use(
     path,
     createProxyMiddleware({
       target: 'http://127.0.0.1:5000/test/',
-      pathRewrite: {
-        [path]: name,
-      },
+      pathRewrite: { [path]: name },
     })
   )
 );
